a302dd3bf46e89aef2fca3e54f68d9b44fe16d19
Saahil <saahilattud@gmail.com>
2024-09-22 00:28:23 +0000

    feat: help cmd

---
+++ src/commands/help.ts
@@ -0,0 +1,15 @@
+import type { Command } from "../types";
+const Util = require("../utils/Utils");
+
+export default {
+  name: "help",
+    description: "Get all cmds ",
+  usage: "<command>",
+  execute(message, args, client) {
+    const embed = Util.Embeds.getDefaultEmbed();
+      embed.setTitle(`Help`);
+      embed.setDescription(client.commands?.map(cmd => `**!${cmd.name}** ${cmd.usage ? `\`${cmd.usage}\``: ""} - ${cmd.description}`).join("\n"));
+      embed.setFooter({ text: `Total Commands: ${client.commands?.size} - Run !help [command] for more info`});
+    message.reply({ embeds: [embed] });
+  },
+} satisfies Command;
